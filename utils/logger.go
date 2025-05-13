4d8369f1022c685a41dfff27795aefbb2ff9a97c
PanzieNuts <trishacarvajal55@gmail.com>
2025-05-13 13:41:37 +0800

    update logger

--- utils/logger.go
+++ utils/logger.go
@@ -1,18 +1,17 @@
+// utils/logger.go
 package utils
 
 import (
-	"log"
-	"os"
+	"github.com/sirupsen/logrus"
 )
 
-var logger *log.Logger
+var logger *logrus.Logger
 
-// InitLogger initializes the logger
 func InitLogger() {
-	logger = log.New(os.Stdout, "", log.LstdFlags)
+	logger = logrus.New()
+	logger.SetLevel(logrus.InfoLevel)
 }
 
-// GetLogger returns the logger instance
-func GetLogger() *log.Logger {
+func GetLogger() *logrus.Logger {
 	return logger
 }
